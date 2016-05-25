45f9a5c34a1827c9b872f5e3bc9f7919a45144ab
Ross Gardiner <ross-public@gmx.com>
2016-05-25 17:21:21 +0100

    Initial commit

---
+++ src/Csp.hs
@@ -0,0 +1,35 @@
+module Csp where
+
+import Text.Show.Functions
+import Data.Set as S
+
+data Event = Ev String deriving (Eq, Ord, Show)
+newtype Process = Process { run :: Event -> Maybe Process}
+                  deriving (Show)
+type Trace = [Event]
+
+stop :: Process
+stop = Process (const Nothing)
+
+coin :: Event
+coin = Ev "coin"
+
+coinAccepter :: Process
+coinAccepter = Process (\x -> case x of
+                                (Ev "coin") -> Just stop
+                                _           -> Nothing)
+
+prefix :: Event -> Process -> Process
+prefix c p = Process (\x -> if x == c then Just p else Nothing)
+
+(⌒) :: Trace -> Trace -> Trace
+(⌒) = (++)
+
+catenate :: Trace -> Trace -> Trace
+catenate = (⌒)
+
+(↾) :: Trace -> S.Set Event -> Trace
+(↾) t es = Prelude.filter (`S.member` es) t
+
+restrict :: Trace -> S.Set Event -> Trace
+restrict = (↾)
