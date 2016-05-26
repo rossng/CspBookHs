9654beff04d76cf412ee98668ff686b39d8064d6
Ross Gardiner <ross-public@gmx.com>
2016-05-26 23:01:53 +0100

    Add the greedy customer and simple vending machine examples

--- src/Csp/Examples.hs
+++ src/Csp/Examples.hs
@@ -1,6 +1,7 @@
 module Csp.Examples where
 
 import Csp.Csp as Csp
+import Data.Map.Strict as Map
 
 stop :: Process
 stop = Process (const Nothing)
@@ -20,3 +21,22 @@ clock :: Process
 clock = Process (\x -> case x of
                           (Ev "tick") -> Just clock
                           _           -> Nothing)
+
+-- a greedy customer will always accept a toffee or chocolate without paying. Else,
+-- he will pay a coin, but will then only accept a chocolate.
+greedyCustomer :: Process
+greedyCustomer = Process (choice $ Map.fromList
+                                    [(Ev "toffee", greedyCustomer)
+                                     (Ev "choc", greedyCustomer)
+                                     (Ev "coin", greedyCustomer')])
+                                     where greedyCustomer' = Process (choice $ Map.fromList [(Ev "choc", greedyCustomer)])
+
+vendingMachine :: Process
+vendingMachine = Process (\x -> case x of
+                                  (Ev "coin") -> Just (choice $
+                                                          Map.fromList [(Ev "choc", vendingMachine)
+                                                                      , (Ev "toffee", vendingMachine)])
+                                  _           -> Nothing)
+
+greedyCustomerAndVendingMachine :: Process
+greedyCustomerAndVendingMachine = greedyCustomer Csp.|| vendingMachine
